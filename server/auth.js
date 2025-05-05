df86ef7edd6af10b91c2cc35e5ffeb9bf0200b33
Mchrispin <mchrispin14@gmail.com>
2025-05-05 12:50:54 +0200

    added JWT key in prod

--- server/auth.js
+++ server/auth.js
@@ -14,7 +14,7 @@ function verifyToken(req, res, next) {
 
     try {
         const secret = process.env.JWT_SECRET || 'secret'
-        console.log("Secret: "+ secret)
+        // console.log("Secret: "+ secret)
         const decoded = jwt.verify(token, secret)
         req.user = decoded // Attach userId to request object
         next() // Continue to the next middleware or route handler
